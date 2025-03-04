345e3a264e0616e79cb1ad40a3eb532fff1b9dc3
Bosabom <nik.13bratischenko@gmail.com>
2025-03-04 19:14:21 +0100

    refactoring and change comments to english

--- 15_game.js
+++ 15_game.js
@@ -1,179 +1,179 @@
- //инициализация глобальных переменных
- var moves = 0;//количество ходов изначально равно 0
- var table; 
- var rows; 
- var columns;
- var textMoves;
- var NumsInTable;
- function Start()
-   {
-    var button = document.getElementById("newGame");
-    button.addEventListener( "click", StartNewGame, false );
-    textMoves = document.getElementById("moves");
-    table = document.getElementById("table");
-    rows = 4;
-    columns = 4;
-    StartNewGame();
+//init of global variables
+var moves = 0;
+var table;
+var rows;
+var columns;
+var textMoves;
+var NumsInTable;
+
+//main function
+function Start() {
+  var button = document.getElementById("newGame");
+  button.addEventListener("click", StartNewGame, false);
+  textMoves = document.getElementById("moves");
+  table = document.getElementById("table");
+  rows = 4;
+  columns = 4;
+  StartNewGame();
+}
+
+//start new game
+function StartNewGame() {
+  var SequenceOfNums = new Array();//array of unique numbers (0-15)
+
+  //array of 16 elements to be sure is certain number was used or not
+  var ArrayWithAlreadyUsedNumbers = new Array(rows * columns);
+  var randomNumber = 0;
+  var count = 0;
+  moves = 0;//moves counter reset
+  textMoves.innerHTML = moves;
+  NumsInTable = new Array(rows);
+
+  //4*4 array for display numbers
+  for (var i = 0; i < rows; i++) {
+    NumsInTable[i] = new Array(columns);
   }
-  function StartNewGame()//начало новой игры
-{
-   var SequenceOfNums = new Array();//массив уникальных чисел от 0 до 15
-   var ArrayWithAlreadyUsedNumbers = new Array( rows * columns );//инициализация массива на 16 элементов для проверки использовалось ли каждое число уже или нет 
-   var randomNumber = 0;
-   var count = 0;
-   moves = 0;//обнуление количества ходов
-   textMoves.innerHTML = moves;
-   NumsInTable = new Array(rows);
-   for (var i = 0; i < rows; i++)
-   {
-     NumsInTable[i] = new Array(columns);//двумерный массив 4 на 4 для отображения чисел в игровом поле
-   }
-   for (var i = 0; i < rows * columns; i++)
-   {
-     ArrayWithAlreadyUsedNumbers[i] = 0;//сначала указывем, что числа еще не использовались
-   }
-   for (var i = 0; i < rows * columns; i++)
-   {
-     randomNumber = Math.floor(Math.random()*rows * columns);//рандомизация числа и запись в переменную randomnumber
-     // проверка уникальности числа (не использовалось ли оно еще)
-     if (ArrayWithAlreadyUsedNumbers[randomNumber] == 0) 
-     {
-       ArrayWithAlreadyUsedNumbers[randomNumber] = 1;//указываем,что данное число уже использовано
-       SequenceOfNums.push(randomNumber);//добавляем число в массив 
-     }
-     //а если число не уникальное(оно уже было использовано), то ищем дальше пока не будут получены все числа что нужны для игры
-     else 
-     {
-       i--;
-     }
-   }
-   
-   // Заполнение двумерного массива для отображения чисел на игровом поле числами из последовательности чисел SequenceOfNums
-   count = 0;//счетчик для перебора элементов массива SequnceOfNums
-   for (var i = 0; i < rows; i++)
-   {
-      for (var j = 0; j < columns; j++)
-      {
-        NumsInTable[i][j] = SequenceOfNums[count];
-        count++;
+
+  for (var i = 0; i < rows * columns; i++) {
+    ArrayWithAlreadyUsedNumbers[i] = 0;//numbers are not yet used
+  }
+
+  for (var i = 0; i < rows * columns; i++) {
+    randomNumber = Math.floor(Math.random() * rows * columns);//randomising of number
+
+    // unique check
+    if (ArrayWithAlreadyUsedNumbers[randomNumber] == 0) {
+      ArrayWithAlreadyUsedNumbers[randomNumber] = 1;//number has been already used
+      SequenceOfNums.push(randomNumber);//add number to array
+    }
+    //continue searching
+    else {
+      i--;
+    }
+  }
+
+  //fill array for displaying on screen
+  count = 0;
+  for (var i = 0; i < rows; i++) {
+    for (var j = 0; j < columns; j++) {
+      NumsInTable[i][j] = SequenceOfNums[count];
+      count++;
+    }
+  }
+
+  ShowTable();
+}
+
+//render game field
+function ShowTable() {
+  var DisplayTiles = "";
+
+  //display grid
+  for (var i = 0; i < rows; i++) {
+    DisplayTiles += "<tr>";
+    for (var j = 0; j < columns; j++) {
+      //empty cell check
+      if (NumsInTable[i][j] == 0) {
+        DisplayTiles += "<td class=\"blank\"> </td>";
+      }
+      else {
+        //display number 1-15 and call function for moving this cell
+        DisplayTiles += "<td class=\"tile\" onclick=\"MoveThisTile(" + i + ", " + j + ")\">" + NumsInTable[i][j] + "</td>";
       }
     }
-    ShowTable();
+    DisplayTiles += "</tr>";
   }
-//функция для отрисовки игрового поля
-  function ShowTable()
-  { 
-    var DisplayTiles = "";
-    //в циклах проходимся по числам в двумерном массиве для отображения игрового поля
-    for (var i = 0; i < rows; i++)
-    {
-      DisplayTiles += "<tr>";
-      for (var j = 0; j < columns; j++)
-      {
-        if (NumsInTable[i][j] == 0)//если число в массиве равно 0, то это пустая клетка
-       {
-          DisplayTiles += "<td class=\"blank\"> </td>";//отображение пустой клетки
-        }
-        else
-        {/*отображение клетки с числом отличным от нуля, а также по клику на клетку
-          будет вызвана функция для передвижения клетки */
-          DisplayTiles += "<td class=\"tile\" onclick=\"MoveThisTile(" + i + ", " + j + ")\">" + NumsInTable[i][j] + "</td>";
-        }
-      } 
-      DisplayTiles += "</tr>";
-    } 
-    
-    table.innerHTML = DisplayTiles;
+
+  table.innerHTML = DisplayTiles;
+}
+
+//cell movement
+function MoveThisTile(tableRow, tableColumn) {
+  //check if we can move this cell
+  if (CanThisTileBeMoved(tableRow, tableColumn, "up") ||
+    CanThisTileBeMoved(tableRow, tableColumn, "down") ||
+    CanThisTileBeMoved(tableRow, tableColumn, "left") ||
+    CanThisTileBeMoved(tableRow, tableColumn, "right")) {
+    incrementNumOfMoves();
   }
-//функция для передвижения клетки,принимает 2 параметра - номера строки и столбца, где находится клетка в таблице
-  function MoveThisTile( tableRow, tableColumn)
-  {
-    //проверка на то,может ли клетка вообще быть передвинутой в одном из 4 направлениях
-    if (CanThisTileBeMoved(tableRow, tableColumn, "up") ||
-        CanThisTileBeMoved(tableRow, tableColumn, "down") ||
-       CanThisTileBeMoved(tableRow, tableColumn, "left") ||
-       CanThisTileBeMoved(tableRow, tableColumn, "right") )
-   {
-     incrementNumOfMoves();//вызов функции по увеличению ходов
-   }
-   else//в противном случае будет вызвано сообщение о том что данная клетка не может быть сдвинутой
-   {
-     alert("Warning! You can't move this tile because tile isn't next to a blank space!\nYou should be more attentive!");
-   }
-   //проверка на конец игры, если это так то будет вызвано сообщение о конце игры и потом игра будет начата заново  
-   if (ThisIsTheEndOrNot())
-   {
-     alert("Congratulations! For solving this puzzle you spent " + moves + " moves.");
-     StartNewGame();
-   }
- }
- //функция по увеличению ходов
- function incrementNumOfMoves()
- {
-   moves++;
-   if (textMoves)
-   {
-     textMoves.innerHTML = moves;
-   }
- } 
- /*функция,которая проверяет, может ли клетка быть передвинутой и если да, то она будет передвинута. 
- Параметры функции: координата строки, координата столбца и направление.*/
- function CanThisTileBeMoved(rowCoordinate, columnCoordinate, direction)
- {
-   rowOffset = 0;//сдвиг по оси ox
-   columnOffset = 0;//сдвиг по оси oy
-  //проверки по направлениям и куда будет произведен сдвиг
-   if (direction == "up")
-   {
-     rowOffset = -1;
-   }
-   else if (direction == "down")
-   {
-     rowOffset = 1;
-   }
-   else if (direction == "left")
-   {
-     columnOffset = -1;
-   }
-   else if (direction == "right")
-   {
-     columnOffset = 1;
-   }  
-   //вначале проверки на то, будет ли сдвинутая клетка находиться в пределах игрового поля
-   if (rowCoordinate + rowOffset >= 0 && columnCoordinate + columnOffset >= 0 &&
-     rowCoordinate + rowOffset < rows && columnCoordinate + columnOffset < columns
-   )
-   {//если клетка не выходит за пределы игрового поля то идет следующая проверка:
-   // пытается ли эта клетка занять место пустой клетки?
-     if ( NumsInTable[rowCoordinate + rowOffset][columnCoordinate + columnOffset] == 0)
-     {
-       NumsInTable[rowCoordinate + rowOffset][columnCoordinate + columnOffset] = NumsInTable[rowCoordinate][columnCoordinate];//клетка занимает положение пустой клетки
-       NumsInTable[rowCoordinate][columnCoordinate] = 0;//клетка которая освободилась стает пустой
-       ShowTable();//показ таблицы, но уже обновленной(т.к произошел сдвиг клетки)
-       return true;//возврат true если клетка передвинулась
-     }
-   }
-   return false; //клетка не сдвинулась
- }
-//функция для определения конца игры
- function ThisIsTheEndOrNot()
- {
-   var right_sequence = 1;//переменная для определения правильной (от 1 до 15) последовательности чисел
-   //в циклах проходимся по двумерному массиву с числами
-   for (var i = 0; i < rows; i++)
-   {
-     for (var j = 0; j < columns; j++)
-     {
-       if (NumsInTable[i][j] != right_sequence)//не равно ли число с игрового поля числу из правильной последовательности
-       {//пустая клетка не находится в правом нижнем углу и количество элементов правильной последовательности не равно 16
-         if ( !(right_sequence === rows * columns && NumsInTable[i][j] === 0 ))
-         {
-           return false;//игра еще не завершена
-         }
-       }
-       right_sequence++;//получение следующего числа из правильной последовательности
-     }
-   }
-   return true;//конец игры
- }
- window.addEventListener( "load", Start, false ); // Вызов функции start() при открытии окна
+  else {
+    alert("Warning! You can't move this tile because tile isn't next to a blank space!\nYou should be more attentive!");
+  }
+  //check for endgame  
+  if (ThisIsTheEndOrNot()) {
+    alert("Congratulations! For solving this puzzle you spent " + moves + " moves.");
+    StartNewGame();
+  }
+}
+
+//incrementing number of moves
+function incrementNumOfMoves() {
+  moves++;
+  if (textMoves) {
+    textMoves.innerHTML = moves;
+  }
+}
+
+/*check if certain cell can be moved*/
+function CanThisTileBeMoved(rowCoordinate, columnCoordinate, direction) {
+  rowOffset = 0;//ox
+  columnOffset = 0;//oy
+
+  //check where need to move this cell
+  if (direction == "up") {
+    rowOffset = -1;
+  }
+  else if (direction == "down") {
+    rowOffset = 1;
+  }
+  else if (direction == "left") {
+    columnOffset = -1;
+  }
+  else if (direction == "right") {
+    columnOffset = 1;
+  }
+
+  //ensure that cell will be inside of game field
+  if (rowCoordinate + rowOffset >= 0 && columnCoordinate + columnOffset >= 0 &&
+    rowCoordinate + rowOffset < rows && columnCoordinate + columnOffset < columns
+  ) {
+    //should this cell be moved on place of empty cell
+    if (NumsInTable[rowCoordinate + rowOffset][columnCoordinate + columnOffset] == 0) {
+      //current cell will take place on empty cell
+      NumsInTable[rowCoordinate + rowOffset][columnCoordinate + columnOffset] = NumsInTable[rowCoordinate][columnCoordinate];
+      //this cell will be empty now
+      NumsInTable[rowCoordinate][columnCoordinate] = 0;
+      //render table after moving cell
+      ShowTable();
+      //cell was sucessfull moved
+      return true;
+    }
+  }
+
+  //cell was not moved
+  return false;
+}
+
+//endgame check
+function ThisIsTheEndOrNot() {
+  var right_sequence = 1;//needs to determine right (1-15) sequence of numbers
+
+  for (var i = 0; i < rows; i++) {
+    for (var j = 0; j < columns; j++) {
+      //check conditions for endgame (all numbers in correct order from 1 to 15)
+      if (NumsInTable[i][j] != right_sequence) {
+        if (!(right_sequence === rows * columns && NumsInTable[i][j] === 0)) {
+          //the game is not ended
+          return false;
+        }
+      }
+      //receive next number of correct sequence (in correct order)
+      right_sequence++;
+    }
+  }
+
+  //end of the game
+  return true;
+}
+
+window.addEventListener("load", Start, false);
